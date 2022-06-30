5000fc67606ed70cfaf38e75c9b8338a8b31c9e3
HEEKWON <higun295@gmail.com>
2022-06-30 17:04:47 +0900

    20220630_2

--- Chapter2/Form1.Designer.cs
+++ Chapter2/Form1.Designer.cs
@@ -29,13 +29,52 @@ namespace Chapter2
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnShowMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.lblMessage.ForeColor = System.Drawing.Color.Brown;
+            this.lblMessage.Location = new System.Drawing.Point(163, 220);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(2, 65);
+            this.lblMessage.TabIndex = 0;
+            // 
+            // btnShowMessage
+            // 
+            this.btnShowMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.btnShowMessage.Location = new System.Drawing.Point(10, 10);
+            this.btnShowMessage.Name = "btnShowMessage";
+            this.btnShowMessage.Size = new System.Drawing.Size(583, 158);
+            this.btnShowMessage.TabIndex = 1;
+            this.btnShowMessage.Text = "Show Message";
+            this.btnShowMessage.UseVisualStyleBackColor = true;
+            this.btnShowMessage.Click += new System.EventHandler(this.btnShowMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnShowMessage);
+            this.Controls.Add(this.lblMessage);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My First Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnShowMessage;
     }
 }
 
--- Chapter2/Form1.cs
+++ Chapter2/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Chapter2
@@ -15,6 +8,11 @@ namespace Chapter2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnShowMessage_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = $"{lblMessage.BorderStyle}";
         }
     }
 }
